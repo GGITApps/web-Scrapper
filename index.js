bb8ba9ecb8b6bd3f50a81e8f04185204b6cc9ae6
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-07-21 02:25:49 -0500

    FInallyyyy scrapper debugueado con nrcs,{capacidad, inscritos}

--- index.js
+++ index.js
@@ -1,65 +1,68 @@
-// const puppeteer = require('puppeteer');
+ const puppeteer = require('puppeteer');
 
-
-
-const HCCrawler = require('headless-chrome-crawler');
- 
-(async () => {
-  const crawler = await HCCrawler.launch({
-    // Function to be evaluated in browsers
-    evaluatePage: (() => ({
-      title: $('.main-container'),
-    })),
-    // Function to be called with evaluated results from browsers
-    onSuccess: (result => {
-      console.log(result);
-    }),
-  });
-  // Queue a request
-  await crawler.queue('https://registroapps.uniandes.edu.co/oferta_cursos/index.php/courses?prefix=CBCO&term=201820&ptrm=1');
-  await crawler.onIdle(); // Resolved when no queue is left
-  await crawler.close(); // Close the crawler
-})();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
 let scrape = async (suburl) => {
-    // const browser = await puppeteer.launch({headless: false});
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false});
+    // const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto('https://registroapps.uniandes.edu.co/oferta_cursos/index.php/courses?prefix='+suburl+"&term=201820&ptrm=1");
+    await page.goto('https://registroapps.uniandes.edu.co/oferta_cursos/index.php/courses?prefix='+suburl+"&term=201820&ptrm=1", {waitUntil: 'networkidle0'});
     const result = await page.evaluate(() => {
 
-        const data = [];
-        const elements = document.querySelectorAll('#main-container > div.row');
-        return elements; // PARA DEBUGGING 
-        for (let element of elements){
-            const capacidad = element.childNodes[1].textContent;
-            const disponible = element.childNodes[3].textContent;
-
-            data.push({capacidad, disponible});
+        
+        const valoresCupos = document.querySelectorAll('.collapsible-body.grey.lighten-3 > div.row > div.col.s4.m4  ');
+        const spanList = document.querySelectorAll('.col.s12.m12.l12 > .row > .col.s12.m5.l3 > span ');
+        const spanS =[];
+        let contador= 0;
+        for (let span of spanList){
+          let spancito = span.innerHTML.trim();
+          contador++;
+          if(contador==4){
+            spanS.push(spancito);
+            
+          }
+          if(contador==9){
+            contador=0;
+          }
+          
         }
-
-        return data;
+        // return elements;
+        
+        // PARA DEBUGGING 
+        
+        let something = [];
+        for (let element of valoresCupos){
+        let elemento= element.innerHTML.trim();
+         something.push(elemento);
+          
+        }
+        return [something,spanS]; 
+        
+        
     });
 
     browser.close();
     return result;
 };
 
+
+
 scrape("CBCO").then((value) => {
-    console.log(value);
-}); */
+  
+  let nrcs= value[1];
+  const data = [];
+  for(i =0; i<value[0].length; i+=3){
+    let temparray = value[0].slice(i,i+3);
+    let capacidad = temparray[0].split(" ")[1];
+    let disponible = temparray[2].split(" ")[1];
+
+    data.push({capacidad, disponible});
+  }
+ 
+  var resultado=[];
+  for(i=0; i<data.length; i++){
+    let nrc = nrcs[i];
+    let dataS = data[i]
+    resultado.push({nrc,dataS});
+  }
+  console.log(resultado)
+});
